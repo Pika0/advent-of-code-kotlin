ea9a70a94b16620d10dfa3b70633ba053f7a50a8
Pika <andrew@iMac-Andrew.local>
2024-12-04 22:39:16 -0500

    added input downloader.

--- src/Utils.kt
+++ src/Utils.kt
@@ -1,4 +1,9 @@
+@file:Suppress("unused")
+
+import java.io.File
 import java.math.BigInteger
+import java.net.HttpURLConnection
+import java.net.URI
 import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readText
@@ -6,7 +11,45 @@ import kotlin.io.path.readText
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = Path("src/$name.txt").readText().trim().lines()
+fun readInput(name: String) = Path("src/${name}.txt").readText().trim().lines()
+
+
+/**
+ * downloads the input from the website if needed
+ */
+fun loadAndReadInput(day: Int, year: Int): List<String> {
+    val name = "day${day.toString().padStart(2, '0')}"
+    val inputFileName = "src/${name}_input.txt"
+    val inputFile = File(inputFileName)
+    if (!inputFile.exists()) {
+
+        val sessionFile = File("src/session.txt")
+        require(sessionFile.exists()) { "Session token file not found at $sessionFile" }
+        val sessionToken = sessionFile.readText().trim()
+
+        val uri = URI("https://adventofcode.com/$year/day/$day/input")
+        val url = uri.toURL()
+        val connection = url.openConnection() as HttpURLConnection
+        connection.setRequestProperty("Cookie", "session=$sessionToken")
+        connection.setRequestProperty(
+            "User-Agent",
+            "Andrew's input grabber in Kotlin, kacheek30@gmail.com"
+        )
+        try {
+            connection.connect()
+            check(connection.responseCode == 200) { "${connection.responseCode} ${connection.responseMessage}" }
+            connection.inputStream.use { input ->
+                inputFile.outputStream().use { output ->
+                    input.copyTo(output)
+                }
+                inputFile.setReadOnly()
+            }
+        } finally {
+            connection.disconnect()
+        }
+    }
+    return readInput("${name}_input")
+}
 
 /**
  * Converts string to md5 hash.
